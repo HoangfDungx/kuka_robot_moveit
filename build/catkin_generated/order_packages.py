6eef937f3cf5f5cc2b6485499134b93b17a547ba
HOANG DUNG <hoangdung@ubuntu.ubuntu-domain>
2021-08-12 10:33:38 +0700

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/hoangdung/ROS_project/industrial_robot_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/hoangdung/ROS_project/industrial_robot_ws/devel;/opt/ros/noetic'.split(';') if '/home/hoangdung/ROS_project/industrial_robot_ws/devel;/opt/ros/noetic' != '' else []
---
+++ build/moveit_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hoangdung/ROS_project/industrial_robot_ws/src/moveit_pkg
+# Build directory: /home/hoangdung/ROS_project/industrial_robot_ws/build/moveit_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
